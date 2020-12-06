3629e819a88d4b2a0ac559952d95f1c605527cff
a528596516 <61381223+a528596516@users.noreply.github.com>
2020-12-06 10:46:48 +0800

    Update adbyby_host.sh

--- trunk/user/adbyby/adbyby_host.sh
+++ trunk/user/adbyby/adbyby_host.sh
@@ -1,6 +1,4 @@
 #一行一个地址，不需要的在前面加 "#"号注释，后台会自动下载，合并，去重。不建议太多，以免误杀严重！
 #此hosts规则是加载到dnsmasq的。
-#yhost
-https://cdn.jsdelivr.net/gh/VeleSila/yhosts/hosts
-#tvbox
-https://dev.tencent.com/u/shaoxia1991/p/yhosts/git/raw/master/data/tvbox.txt
+#依然的爱
+https://gitee.com/xueweihan/codes/6g793pm2k1hacwfbyesl464/raw?blob_name=GitHub520.yml
